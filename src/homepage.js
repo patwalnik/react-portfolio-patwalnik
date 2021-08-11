39e9a1dc2a24300a3b746105bfe0e44404244def
Saurabh Patwal <pro.patwal@gmail.com>
2021-08-12 03:55:24 +0530

    github and linkedin url added

--- src/homepage.js
+++ src/homepage.js
@@ -6,7 +6,18 @@ export const HomePage = () => {
     <div className="homepage">
       <h1>Hey Fellas,</h1>
       <DraggableList items={'My name is saurabh patwal'.split(' ')} />
-      {/* <DraggableList items={'Lorem ipsum dolor sit'.split(' ')} /> */}
+      <h2>
+        github :{' '}
+        <a href="https://github.com/patwalnik" target="_blank">
+          https://github.com/patwalnik
+        </a>
+      </h2>
+      <h2>
+        linkedin :{' '}
+        <a href="https://www.linkedin.com/in/patwalnik/" target="_blank">
+          https://www.linkedin.com/in/patwalnik/
+        </a>
+      </h2>
     </div>
   )
 }
